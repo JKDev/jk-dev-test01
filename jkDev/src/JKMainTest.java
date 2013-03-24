2766801c567fac39b6b77a16e7f73a96ea204c42
speedman00@naver.com <speedman00@naver.com>
2013-03-24 17:19:54 +0900

    1111

---
+++ jkDev/src/JKMainTest.java
@@ -0,0 +1,14 @@
+
+public class JKMainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println(" JK Test DDDDDDDDDDDDDDDDDDDDDDDDDDD");
+	}
+
+}
