9e738525d949433c89ee1550b4663e3bf916ebb8
speedman00@naver.com <speedman00@naver.com>
2013-04-08 23:04:07 +0900

    1

--- jkDev/src/JKMainTest.java
+++ jkDev/src/JKMainTest.java
@@ -6,7 +6,7 @@ public class JKMainTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println( "12=1111=============== 2013-04-09 =========================" );
+		System.out.println( "111112=1111=============== 2013-04-09 =========================" );
 		
 		System.out.println(" JK Test DDDDDDDDDDDDDDDDDDDDDDDDDDD");
 	}
